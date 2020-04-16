087c34f7cec279c9e83e312ed33fa87341be3994
ULUL AZMI <1710510160@stmikbumigora.ac.id>
2020-04-17 05:46:50 +0800

    KEDUA Kalinya

--- app/src/main/java/com/example/blackactivity/MainActivity.java
+++ app/src/main/java/com/example/blackactivity/MainActivity.java
@@ -3,23 +3,30 @@ package com.example.blackactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etInput;
+    private EditText etInput1;
+    private EditText etInput2;
+    private EditText etInput3;
     private TextView tvOutput;
+    private String hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etInput = findViewById(R.id.etInput);
+        etInput1 = findViewById(R.id.etInputNim);
+        etInput2 = findViewById(R.id.etInputNama);
+        etInput3 = findViewById(R.id.etInputAlamat);
         tvOutput = findViewById(R.id.tvOutput);
+
     }
     public void BtnTampilkan(View v){
-        tvOutput.setText(etInput.getText());
+        tvOutput.setText("NIM\t:"+etInput1.getText()+"\n"+"NAMA\t:"+etInput2.getText()+"\n"+"ALAMAT\t"+etInput3.getText());
     }
 }
